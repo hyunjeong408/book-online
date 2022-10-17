686d575d1b91153a0b17880bd177ca6ce89f4b63
hyunjeong <65072995+hyunjeong408@users.noreply.github.com>
2022-10-17 20:17:02 +0900

    feat: Main blocks 추가 (#4)
    * feat: 메인페이지 세블럭 추가

---
+++ src/components/main-blocks/ReviewBox.tsx
@@ -0,0 +1,16 @@
+import React from "react";
+import './main-blocks-css/ReviewBox.css'
+
+export default () => {
+    return (
+        <div className="review-box">
+            <p className="box-title">
+                비평: 이달의 책
+            </p>
+            <div className="book-of-the-month">
+                Here
+            </div>
+
+        </div>
+    )
+}
