bc46f71df6bf9878917b3e52306f2d8fa7d9a9ef
hyunjeong408 <gnlgus48@g.skku.edu>
2022-10-17 20:41:02 +0900

    feat: route(페이지 이동) 추가

--- src/components/Menu.tsx
+++ src/components/Menu.tsx
@@ -1,4 +1,8 @@
 import React from "react";
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import App from "../App";
+import { Link } from "react-router-dom";
+
 import './css/Menu.css'
 import * as data from './links.json'
 
@@ -15,16 +19,30 @@ const Links: React.FC<{links: Link[]}> = ({links}) => {
         <div className='links-container'>
             {links.map((link: Link)=>{
                 return(
-                    <div key={link.href} className='link'>
-                        <a href={link.href}>
-                            {link.label}
-                        </a>
-                    </div>
+                    <Link className="link" to={link.href}>{link.label}</Link>
+                    // <div key={link.href} className='link'>
+                    //     <a href={link.href}>
+                    //         {link.label}
+                    //     </a>
+                    // </div>
                 )
             })}
         </div>
     )
 }
+
+// const Links = () => {
+//     return(
+//         <div className='links-container'>
+//             <Link className="link" to="/today">Invoices</Link>
+//             <Link className="link" to="/new">Expenses</Link>
+//             <Link className="link" to="/best">Invoices</Link>
+//             <Link className="link" to="/expenses">Expenses</Link>
+//             <Link className="link" to="/invoices">Invoices</Link>
+//             <Link className="link" to="/expenses">Expenses</Link>
+//         </div>
+//     )
+// }
 
 const Menu = () => {
     return(
