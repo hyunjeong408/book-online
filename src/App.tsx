8230f12b92284af3c8e4e28a69b5a5efd822ab38
hyunjeong408 <gnlgus48@g.skku.edu>
2022-12-15 19:01:05 +0900

    feat: back-end 연결

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,22 @@
-import React from 'react';
+import React, {useState, useEffect} from 'react';
 import './App.css';
 import Layout from './components/Layout';
 
 function App() {
+  const [message, setMessage]=useState([]);
+  useEffect(()=>{
+    fetch("/api/demo-web")
+        .then((response)=>{
+          return response.json();
+        })
+        .then((data)=>{
+            setMessage(data);
+        });
+  },[]);
   return (
     <div className="App">
       <Layout category='Home'/>
+      {message}
     </div>
   );
 }
