da2c08f10e9f62770af497c26392c1102fe87dcf
hyunjeong408 <gnlgus48@g.skku.edu>
2023-02-06 14:42:02 +0900

    feat: 로그인 spring security+jwt 백엔드 연동과 localStorage에 따른 프론트 변경

--- src/App.tsx
+++ src/App.tsx
@@ -3,20 +3,21 @@ import './App.css';
 import Layout from './components/Layout';
 
 function App() {
-  const [message, setMessage]=useState([]);
-  useEffect(()=>{
-    fetch("/api/demo-web")
-        .then((response)=>{
-          return response.json();
-        })
-        .then((data)=>{
-            setMessage(data);
-        });
-  },[]);
+  // const [message, setMessage]=useState([]);
+  // useEffect(()=>{
+  //   fetch("/api/demo-web")
+  //       .then((response)=>{
+  //         return response.json();
+  //       })
+  //       .then((data)=>{
+  //           setMessage(data);
+  //       });
+  // },[]);
+  console.log("App Here");
   return (
     <div className="App">
       <Layout category='Home'/>
-      {message}
+      {/* {message} */}
     </div>
   );
 }
--- src/pages/Login.tsx
+++ src/pages/Login.tsx
@@ -4,15 +4,17 @@ import { REST_API_KEY, REDIRECT_URI } from '../kakaoLoginInfo.js'
 import Header from '../components/Header';
 import Footer from '../components/Footer';
 import Modal from 'react-modal';
-import { response } from 'express';
+import {useNavigate} from "react-router-dom";
 import axios from "axios";
 
 function Login() {
   const KAKAO_AUTH_URI = `https://kauth.kakao.com/oauth/authorize?response_type=code&client_id=${REST_API_KEY}&redirect_uri=${REDIRECT_URI}`
+  const navigate = useNavigate();
   const handleLogin = () => {
     window.location.href = KAKAO_AUTH_URI;
   }
   let [joinModalOpen, setJoinModalOpen] = useState(false);
+  let [loginErr, setLoginErrMsg] = useState(false);
   let [id, setId] = useState("");
   let [pw, setPw] = useState("");
   let [name, setName] = useState("");
@@ -26,12 +28,31 @@ function Login() {
     setName(e.target.value);
   }
   const clickLoginBtn = () => {
-    console.log(id, pw);
+    axios.post('/auth/login', {
+      user_email: id,
+      user_pw: pw
+    })
+    .then((res)=>{
+        setLoginErrMsg(false);
+        console.log(res.data.accessTokenExpiresIn);
+        localStorage.clear();
+        localStorage.setItem('token', res.data.accessToken);
+        localStorage.setItem('expirationTime', res.data.accessTokenExpiresIn);
+        navigate('/home')
+    })
+    .catch((err)=>{
+      if(err.response.status === 401){
+        console.log("401 Error");
+
+      }
+      setLoginErrMsg(true);
+    })
+    setJoinModalOpen(false);
   }
   const clickJoinBtn = () => {
-    axios.post('/login', {
+    axios.post('/auth/join', {
       user_name: name,
-      user_id: id,
+      user_email: id,
       user_pw: pw
     })
     .then(function(response){
@@ -46,17 +67,24 @@ function Login() {
   return (
     <div className={styles['layout']}>
         <Header category="로그인"/>
-        <Modal isOpen={joinModalOpen} className={styles['joinModalBox']}>
+        <Modal isOpen={joinModalOpen} className={styles['joinModalBox']} ariaHideApp={false}>
           <p>회원가입</p>
           <input type="text" onChange={inputName} placeholder="이름"></input>
           <input type="text" onChange={inputID} placeholder="아이디"></input>
           <input type="password" onChange={inputPW} placeholder="비밀번호"></input>
-          <button onClick={clickJoinBtn}>Modal Open</button>
+          <div className={styles['btnBox']}>
+            <button onClick={clickJoinBtn}>가입하기</button>
+            <button onClick={()=>setJoinModalOpen(false)}>닫기</button>  
+          </div>
+          
         </Modal>
         <div className={styles['mainBox']}>
             <div className={styles['loginBox']}>
             <input type="text" onChange={inputID} className={styles['inputBox']} placeholder="아이디"/>
             <input type="password" onChange={inputPW} className={styles['inputBox']} placeholder="비밀번호"/>
+            {
+              (loginErr)? <div className={styles['errMsg']}>아이디/비밀번호가 잘못되었습니다.</div>:null
+            }
             <button id="login-btn" onClick={clickLoginBtn} className={styles['loginBtn']}>로그인</button>
             <button id="join-btn" onClick={()=>setJoinModalOpen(true)} className={styles['joinBtn']}>회원가입</button>
           </div>
