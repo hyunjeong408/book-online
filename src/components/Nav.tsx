a1eb29ab92939673a6f7b6c3c67033ec98a89882
hyunjeong <65072995+hyunjeong408@users.noreply.github.com>
2023-02-06 14:42:37 +0900

    feat: 로그인 spring security+jwt 백엔드 연동과 localStorage에 따른 프론트 변경 (#11)

--- src/components/Nav.tsx
+++ src/components/Nav.tsx
@@ -1,41 +1,46 @@
-import React from 'react'
+import React, {useState} from 'react'
 import styles from './css/Nav.module.css'
 import * as data from './links.json'
 import SearchBox from './SearchBox';
-
-const linkString = JSON.stringify(data);
-const links = JSON.parse(linkString).links;
-
-type Link = {
-    label: string;
-    href: string;
-}
-
-const Links: React.FC<{links: Link[]}> = ({links}) => {
-    return (
-        <div className={styles['links-container']}>
-            {links.map((link: Link)=>{
-                return(
-                    <div key={link.href} className={styles['link']}>
-                        <a href={link.href}>
-                            {link.label}
-                        </a>
-                    </div>
-                )
-            })}
-        </div>
-    )
-}
-
+import Modal from 'react-modal';
+import {useNavigate} from "react-router-dom";
 
 function Nav() {
+    const navigate = useNavigate();
+    let [logoutModalOpen, setLogoutModalOpen] = useState(false);
+
+    const logout = ()=>{
+        localStorage.clear();
+        setLogoutModalOpen(false);
+        navigate('/');
+    }
+
+    const login = () => {
+        navigate('/login');
+    }
     return(
         <nav className={styles.navbar}>
+            <Modal isOpen={logoutModalOpen} className={styles['logout-modal']} ariaHideApp={false}>
+                <p>로그아웃 되었습니다.</p>
+                <button className={styles['logout-ok-btn']} onClick={logout}>확인</button> 
+            </Modal>
             <div className={styles['logo-container']}>
-                <img src="assets/logo_txt_color.svg" alt="main-logo"></img>
+                <img src="assets/logo_txt_color.svg" alt="main-logo" onClick={()=>{navigate('/');}}></img>
             </div>
             <div className={styles['side-navbar']}>
-                <Links links={links}/>
+                {/* <Links links={links}/> */}
+                { localStorage.getItem('token')?
+                (<div>
+                    <button onClick={()=>setLogoutModalOpen(true)}>
+                        로그아웃
+                    </button>
+                </div>)
+                :(<div>
+                    <button onClick={login}>
+                        로그인
+                    </button>
+                </div>)
+                }
                 <SearchBox/>
             </div>
         </nav>
